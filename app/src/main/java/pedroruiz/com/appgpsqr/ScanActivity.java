ad0c6d4cd47c3bd1dfffbc66aabbf461e60208e4
Pedro518 <pedro_518@hotmail.com>
2016-02-07 11:22:37 +0100

    Añadida documentación

--- app/src/main/java/pedroruiz/com/appgpsqr/ScanActivity.java
+++ app/src/main/java/pedroruiz/com/appgpsqr/ScanActivity.java
@@ -1,3 +1,18 @@
+/**
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pedroruiz.com.appgpsqr;
 
 import android.content.Intent;
@@ -11,6 +26,14 @@ import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+/**
+ * <p>Clase que se encarga de asociar las acciones del usuario con la ejecución de las ordenes correspondientes</p>
+ *
+ * @author Pedro Antonio Ruiz Cuesta
+ * @author Ignacio Martín Requena
+ * Última modificación: 7/2/2016
+ */
 
 public class ScanActivity extends AppCompatActivity {
 
